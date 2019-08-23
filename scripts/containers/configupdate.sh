b1e0590fb0318091078aa10cb78e9ac26101a92a
James J Moore <James.Moore@ibm.com>
2019-08-23 13:53:15 -0400

    Reachitecture Including Deletes

--- scripts/containers/configupdate.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-translateStart="tr '\n' '\r'"
-translateEnd="tr '\r' '\n'"
-replaceKey="\"APIListen\": \"0.0.0.0:80\",\r"
-replaceValue="\"APIListen\": \"0.0.0.0:80\",\r        \"ExchangeMessageTTL\": 120,\r"
-anaxjsonupdate="cat /etc/horizon/anax.json | ${translateStart} | sed -e 's/${replaceKey}/${replaceValue}/'  | ${translateEnd} > /root/scaler/anax.json"
-eval ${anaxjsonupdate}
--- scripts/harness/cleanold.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#kill any lingering scaler processes that might be present as a result of failures
-for i in $(cat config.json | jq -r '.endpoints | .[]'); do ssh root@$i "hostname & pkill -f scale.anaxscale" ; done
-
-#validate no lingering scaler processes exist
-for i in $(cat config.json | jq -r '.endpoints | .[]'); do ssh root@$i "hostname & ps -ef | grep scale.anaxscale | grep -v grep | awk '{ print $2}'" ; done
-
